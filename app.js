d5fb7372787c817e83bbe938bb125d35164bf7b1
Angelika Kubicka <angkubicka99@gmail.com>
2021-03-04 23:43:13 +0100

    Fading text

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 const header_title = document.querySelector('.header__title');
 const text = header_title.textContent;
-console.log(text);
+const words = text.split('');
+header_title.textContent = '';
 
+for(let i=0; i<words.length; i++) {
+    header_title.innerHTML += "<span>" + words[i] + "</span>";
+}
+
+let timer = setInterval(setClass, 50);
+let char = 0;
+
+function setClass() {
+    let span = header_title.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if(char === words.length) {
+        clear();
+        return;
+    }
+}
+
+function clear() {
+    clearInterval(timer);
+    timer = null;
+}
