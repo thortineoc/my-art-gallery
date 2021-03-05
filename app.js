b80fdc86ab964bbb9c4c9915d0966b2286d183ae
Angelika Kubicka <angkubicka99@gmail.com>
2021-03-05 14:16:34 +0100

    Text transition

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const header_title = document.querySelector('.header__title');
-const text = header_title.textContent;
-const words = text.split('');
+const header_text = header_title.textContent;
+const header_words = header_text.split('');
 header_title.textContent = '';
 
-for(let i=0; i<words.length; i++) {
-    header_title.innerHTML += "<span>" + words[i] + "</span>";
+for(let i=0; i<header_words.length; i++) {
+    header_title.innerHTML += "<span>" + header_words[i] + "</span>";
 }
 
+const header_description = document.querySelector('.header__description');
 let timer = setInterval(setClass, 50);
 let char = 0;
 
 function setClass() {
     let span = header_title.querySelectorAll('span')[char];
-    span.classList.add('fade');
+    span.classList.add('appear');
     char++;
-    if(char === words.length) {
+    if(char === header_words.length) {
         clear();
+        slideAndShow(header_description);
         return;
     }
 }
@@ -23,4 +25,8 @@ function setClass() {
 function clear() {
     clearInterval(timer);
     timer = null;
+}
+
+function slideAndShow(text) {
+    text.classList.add('slide');
 }
